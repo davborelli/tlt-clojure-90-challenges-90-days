bb7cadd8ea2b279978cc78e64df3fe9e2d8d9060
davborelli <david.borelli@nubank.com.br>
2026-05-29 14:57:05 -0300

    feat: implement XML to JSON conversion functionality
    Added the xml->json function to convert XML data structures into JSON format. This includes helper functions for transforming kebab-case keys to camelCase, extracting text content, and handling child elements. A sample usage of the xml->json function is also included for demonstration purposes, enhancing the application's capability to process XML data effectively.

--- challenges/073-xml-to-json/073.clj
+++ challenges/073-xml-to-json/073.clj
@@ -1,8 +1,45 @@
-(ns xml-to-json)
+(ns xml-to-json
+  (:require [clojure.string :as str]))
 
-(defn xml->json
+(declare xml->json)
+
+(defn- kebab->camel 
+  [kebab-keyword]
+  (let [parts (str/split (name kebab-keyword) #"-")
+        first-part (first parts)
+        rest-parts (map clojure.string/capitalize (rest parts))]
+    (keyword (apply str first-part rest-parts))))
+
+(defn- extract-text
+  [content]
+  (let [texts (filter string? content)]
+    (when (seq texts)
+      {:text (str/join " " texts)})))
+
+(defn- extract-children
+  [content]
+  (let [child-elements (filter map? content)]
+    (when (seq child-elements)
+      {:children (mapv xml->json child-elements)})))
+
+(defn xml->json 
   [xml-data]
-  )
+  (let [tag-name      (kebab->camel (:tag xml-data))
+        attrs         (:attrs xml-data)
+        content       (:content xml-data)
+        text-part     (extract-text content)
+        children-part (extract-children content)
+        body (merge
+              (when (seq attrs) {:attributes attrs})
+              text-part
+              children-part)]
+    {tag-name body}))
+
+(println (xml->json
+          {:tag :user-profile
+           :attrs {:id "123"}
+           :content [{:tag :name :attrs {} :content ["John"]}
+                     {:tag :email :attrs {} :content ["john@example.com"]}]}))
 
 (defn- tst []
   (assert (=
